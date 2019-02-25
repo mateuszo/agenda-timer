1cd4cb9045fb2951f9a26fbcc363cf95673fdca6
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2019-02-25 23:08:03 +0100

    Add form component

--- src/App.js
+++ src/App.js
@@ -20,16 +20,40 @@ class App extends Component {
 
 
 class NewItemForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {name: '', duration: 0};
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    event.target.reset();
+    event.preventDefault();
+  }
+
   render() {
     return (
-    <Form inline>
+    <Form inline onSubmit={this.handleSubmit}>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="name" className="mr-sm-2">Name:</Label>
-          <Input type="text" name="name" id="name" placeholder="boring stuff" />
+          <Input type="text" name="name" id="name" placeholder="boring stuff" onChange={this.handleInputChange} />
         </FormGroup>
         <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
           <Label for="duration" className="mr-sm-2">Duration:</Label>
-          <Input type="number" name="duration" id="duration" placeholder="33" />
+          <Input type="number" name="duration" id="duration" placeholder="33" onChange={this.handleInputChange} />
         </FormGroup>
         <Button>Add</Button>
       </Form>
