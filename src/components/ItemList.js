5fdade1ad8fa381d71c5acdde2202d91ebd660cb
Mateusz Ostafil <mateusz.ostafil@codecool.com>
2019-03-11 16:38:22 +0100

    Refactor ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -8,44 +8,52 @@ import { minutesToString } from '../utils/utils';
 import AgendaItem from "../model/AgendaItem";
 
 const styles = theme => ({
-  root: {
-    width: '100%',
-    maxWidth: 360,
-    backgroundColor: theme.palette.background.paper,
-  },
+    root: {
+        width: '100%',
+        maxWidth: 360,
+        backgroundColor: theme.palette.background.paper,
+    },
 });
 
 class ItemList extends Component {
-  constructor(props){
-    super(props);
-    this.classes = props.classes;
-  }
+    constructor(props){
+        super(props);
+        this.classes = props.classes;
+    }
 
-  render() {
+    calculateTotalTime() {
+        return minutesToString(
+            Array.from(this.props.items)
+                .map(([key, item]) => item.duration)
+                .reduce((prev, curr) => prev + curr, 0
+                ));
+    }
 
-    const listItems = Array.from(this.props.items).map(
-      ([key, item]) => <Item key={key} item={item} buttonAction={this.props.deleteItem} buttonType={"delete"} updateItem={this.props.updateItem}/>
-    );
+    renderItemList() {
+        return Array.from(this.props.items).map(
+            ([key, item]) => <Item key={key}
+                                   item={item}
+                                   buttonAction={this.props.deleteItem}
+                                   buttonType={"delete"}
+                                   updateItem={this.props.updateItem}/>
+        );
+    }
 
-    const total = minutesToString(
-      Array.from(this.props.items)
-      .map( ([key, item]) => item.duration)
-      .reduce( (prev, curr) => prev + curr, 0
-    ));
+    render() {
 
-    const newItem = new AgendaItem("", 0);
+        const newItem = new AgendaItem("", 0);
 
-    return (
-      <div className={this.classes.root}>
-        <List component="nav">
-          {listItems}
-          <Item key={newItem.id} item={newItem} buttonAction={this.props.updateItem} buttonType={"save"} updateItem={() => {}} />
-        </List>
+        return (
+            <div className={this.classes.root}>
+                <List component="nav">
+                    {this.renderItemList()}
+                    <Item key={newItem.id} item={newItem} buttonAction={this.props.updateItem} buttonType={"save"} updateItem={() => {}} />
+                </List>
 
-        <div>Total: {total}</div>
-      </div>
-    )
-  };
+                <div>Total: {this.calculateTotalTime()}</div>
+            </div>
+        )
+    };
 }
 
 export default withStyles(styles)(ItemList)
